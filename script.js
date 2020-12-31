b1ac60292c9e88481541a608b1fdd618cb4a59f5
mrlaboratory <admin@mrlaboratory.com>
2020-12-31 18:58:58 +0600

    add more code for this project

--- script.js
+++ script.js
@@ -2,13 +2,10 @@
  $(".inputcc input").val("https:www.youtube.com/channel/UC8uhhaqA9s540gufCwKwrKA")
  $(".inputcc button").on("click",function(){
      var chURL = $(".channelUrl").val();
-     var howMuchVideo = 5;
+     var howMuchVideo = 20;
      var urlsplit= chURL.split(/^.*(channel\/|youtube.com\/user\/[^#]*#([^\/]*?\/)*)\??v?=?([^#\&\?]*).*/);
      var mrApi = "AIzaSyDSwN5XQQfJvYhY4iLMf3DZmUr6MdCjawg";
      var getDataUrl = "https:www.googleapis.com/youtube/v3/search?key="+mrApi+"&channelId="+urlsplit[3]+"&part=snippet,id&order=date&maxResults="+howMuchVideo
-    
-   
-    console.log(getDataUrl)
      $.ajax({
          type: "GET",
         dataType: "json",
@@ -17,8 +14,7 @@
             var video = 0;
          for(var i=0;i<data1.items.length;i++){
           appendf(data1.items[i].id.videoId)
-         }
-        
+         } 
         }
         });
 
